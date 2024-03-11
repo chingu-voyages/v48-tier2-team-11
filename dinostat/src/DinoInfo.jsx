5a8789ae6f87e9b15cb72b18ebce5139834a23af
YasirAzhari <grabuser2000@gmail.com>
2024-03-11 06:58:33 +0400

    Add comments and clean up

--- dinostat/src/DinoInfo.jsx
+++ dinostat/src/DinoInfo.jsx
@@ -4,10 +4,11 @@ import { useParams } from 'react-router-dom';
 export default function DinoInfo() {
   const { dinoId } = useParams();
   const [dinoJson, setDinoJson] = useState()
-  // TODO: add useEffect block that calls api with dinoId and uses response data to se dinoJson
+  // TODO: Add useEffect block that calls api with dinoId and uses response data to set dinoJson
+
   return (
     <div>
-      The id for this dinosaur pulled from the url parameter is {dinoId}
+      The id for this dinosaur pulled from the url parameter is {dinoId} (try changing it)
     </div>
   )
 }
--- dinostat/src/DinoList.jsx
+++ dinostat/src/DinoList.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+
 export default function DinoList() {
-  const [dinoJsonList, setDinoJsonList] = useState([{id: 1}, {id: 2}, {id: 3}, {id: 4}])
-  // TODO: add useEffect block that calls api and uses response data to set dinoJsonList
+  // Holds response data from the api to be fed to each dinosaur card. 
+  const [dinoJsonList, setDinoJsonList] = useState([{id: 1}, {id: 2}, {id: 3}, {id: 4}]) 
+  
+  // TODO: Add useEffect block that calls api and uses response data to set dinoJsonList
 
   return (
     <div>
-      {dinoJsonList.map((x)=><ul><Link to={'/dinosaurs/'+x.id}> {x.id} </Link></ul>)}
+      {dinoJsonList.map((x) => <ul><Link to={'/dinosaurs/'+x.id}> {x.id} </Link></ul>)}
     </div>
   )
 }
