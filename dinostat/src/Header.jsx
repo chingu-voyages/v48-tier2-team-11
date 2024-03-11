57d4aec2c3648659da0fed4fad72b19761f119bd
YasirAzhari <grabuser2000@gmail.com>
2024-03-11 06:29:45 +0400

    Add links to each route in header

--- dinostat/src/Header.jsx
+++ dinostat/src/Header.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <ul>
+        <li><Link to="/"> Home </Link></li>
+        <li><Link to="/dinosaurs"> Dinosaur List </Link></li>
+        <li><Link to="/charts"> Charts </Link></li>
+      </ul> 
+    </div>
   )
 }
