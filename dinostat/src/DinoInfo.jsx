9ff68c2f5aafd4bd6604e10f6641cc15eeb06547
YasirAzhari <grabuser2000@gmail.com>
2024-03-11 06:14:17 +0400

    Add jsx file for each page

---
+++ dinostat/src/DinoInfo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function DinoInfo() {
+  return (
+    <div>DinoInfo</div>
+  )
+}
---
+++ dinostat/src/DinoList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DinoList() {
+  return (
+    <div>DinoList</div>
+  )
+}
