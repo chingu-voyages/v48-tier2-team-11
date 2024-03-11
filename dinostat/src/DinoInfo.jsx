018f56b5f1070b5a698f8efbe6760ad5cb0a4bd8
YasirAzhari <grabuser2000@gmail.com>
2024-03-11 06:49:34 +0400

    Add routes to dino info pages from dino list page

--- dinostat/src/DinoInfo.jsx
+++ dinostat/src/DinoInfo.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function DinoInfo() {
-    const { dinoId } = useParams();
+  const { dinoId } = useParams();
+  const [dinoJson, setDinoJson] = useState()
+  // TODO: add useEffect block that calls api with dinoId and uses response data to se dinoJson
   return (
     <div>
       The id for this dinosaur pulled from the url parameter is {dinoId}
--- dinostat/src/DinoList.jsx
+++ dinostat/src/DinoList.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 export default function DinoList() {
+  const [dinoJsonList, setDinoJsonList] = useState([{id: 1}, {id: 2}, {id: 3}, {id: 4}])
+  // TODO: add useEffect block that calls api and uses response data to set dinoJsonList
+
   return (
-    <div>DinoList</div>
+    <div>
+      {dinoJsonList.map((x)=><ul><Link to={'/dinosaurs/'+x.id}> {x.id} </Link></ul>)}
+    </div>
   )
 }
