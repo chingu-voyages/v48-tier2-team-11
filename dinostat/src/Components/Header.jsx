caad7fbb7567b19e9a1547ccb5782abfdfc2ab43
TerminalC3 <keerthikumar.m10@gmail.com>
2024-03-14 21:38:14 +0530

    Add hamburger menu and app name

--- dinostat/src/Components/Header.jsx
+++ dinostat/src/Components/Header.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
-      <ul>
-        <li><Link to="/"> Home </Link></li>
-        <li><Link to="/dinosaurs"> Dinosaur List </Link></li>
-        <li><Link to="/charts"> Charts </Link></li>
-      </ul>
+      <div className="header">
+        <div className="menu">
+          <button type="button" onClick={toggle}>
+            =
+          </button>
+        </div>
+
+        <div className="app-name">
+          <h1>DinoStat</h1>
+        </div>
+      </div>
+
+      <div className="nav-links" style={{ display: isOpen ? 'block' : 'none' }}>
+        <ul>
+          <li className="link-item"><Link to="/"> Home </Link></li>
+          <li className="link-item"><Link to="/dinosaurs"> Dinosaur List </Link></li>
+          <li className="link-item"><Link to="/charts"> Charts </Link></li>
+        </ul>
+      </div>
+
     </div>
   );
 }
