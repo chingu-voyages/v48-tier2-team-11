6ad7e936de06a9f1f30f7253bb282a794b8df26f
YasirAzhari <grabuser2000@gmail.com>
2024-03-17 16:41:26 +0400

    Add card info and css

--- dinostat/src/Components/DinoCard.jsx
+++ dinostat/src/Components/DinoCard.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function DinoCard() {
+export default function DinoCard({ id, img, title, description }) {
   return (
-    <div>DinoCard</div>
+    <div className="dinoCard">
+      <img src={img} alt="Dinosaur Image" width={200} height={200} />
+      <div>
+        <div className="dinoCard-title">{title}</div>
+        {description.length < 80
+          ? <div className="dinoCard-description">{description}</div>
+          : <div className="dinoCard-description">{description.slice(0, 77)}...</div>}
+        <Link to={`/dinosaurs/${id}`}>
+          <button type="button" className="moreInfo">More info</button>
+        </Link>
+      </div>
+    </div>
   );
 }
