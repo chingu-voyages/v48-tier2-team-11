c13c3393d1b02932e06d3c7e709fb168d989b79f
TerminalC3 <keerthikumar.m10@gmail.com>
2024-03-18 20:43:13 +0530

    Add class names for css manipulation

--- dinostat/src/Components/DinoCard.jsx
+++ dinostat/src/Components/DinoCard.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import missing from '/missing.png';
+import missing from '../../public/missing.png';
 
-export default function DinoCard({ id, img, title, description }) {
+export default function DinoCard({
+  id, img, title, description,
+}) {
   return (
     <div className="dinoCard">
       <img src={img === 'N/A' ? missing : img} alt="Dinosaur" width={200} height={200} />
-      <div>
+      <div className="detail-div">
         <div className="dinoCard-title">{title}</div>
-        {description.length < 80
-          ? <div className="dinoCard-description">{description}</div>
-          : <div className="dinoCard-description">{description.slice(0, 77)}...</div>}
-        <Link to={`/dinosaurs/${id}`}>
-          <button type="button" className="moreInfo">More info</button>
-        </Link>
+        <div className="dinoCard-description">{description}</div>
+        <div className="moreInfo-div">
+          <Link to={`/dinosaurs/${id}`}>
+            <button type="button" className="moreInfo">More info</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
