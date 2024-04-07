f61a0637ac987413a0b90bf9d68d1307d158de2f
TerminalC3 <keerthikumar.m10@gmail.com>
2024-04-07 16:31:43 +0530

    Update

--- dinostat/src/Components/FilterChoose.jsx
+++ dinostat/src/Components/FilterChoose.jsx
@@ -16,52 +16,51 @@ export default function FilterChoose({ setFilterSelect }) {
 
   dinoJsonList.forEach((dino) => (
     Object.keys(filterable).forEach((key) => {
-      if (dino[key]) {
-        let result = dino[key];
-        if (key === 'whenLived') {
-          const keyee = result.split(',');
-          [result] = keyee;
-        }
-        if (!filterable[key].includes(result)) filterable[key].push(result);
+      let result = dino[key];
+      if (key === 'whenLived') {
+        const keyee = result.split(',');
+        [result] = keyee;
       }
+      if (!filterable[key].includes(result)) filterable[key].push(result);
     })
   ));
 
   return (
     <div className="filter-overlay">
       <div className="filter-choose">
-        <h2>Choose filters</h2>
+        <h2 className="filter-choose-header">Choose filters</h2>
+
         <div className="filter-category">
 
           <div className="dino-type">
             <h3>Type</h3>
             {filterable.typeOfDinosaur.map((item) => (
-              <button type="button" key={item} onClick={() => setFilterSelect(item)}>{item}</button>
+              <button type="button" key={item} onClick={() => setFilterSelect(`typeOfDinosaur: ${item}`)}>{item}</button>
             ))}
           </div>
 
           <div className="dino-diet">
             <h3>Diet</h3>
             {filterable.diet.map((item) => (
-              <button type="button" key={item} onClick={() => setFilterSelect(item)}>{item}</button>
+              <button type="button" key={item} onClick={() => setFilterSelect(`diet: ${item}`)}>{item}</button>
             ))}
           </div>
           <div className="dino-lived">
             <h3>Period</h3>
             {filterable.whenLived.map((item) => (
-              <button type="button" key={item} onClick={() => setFilterSelect(item)}>{item}</button>
+              <button type="button" key={item} onClick={() => setFilterSelect(`whenLived: ${item}`)}>{item}</button>
             ))}
           </div>
           <div className="dino-country">
             <h3>Country</h3>
             {filterable.foundIn.map((item) => (
-              <button type="button" key={item} onClick={() => setFilterSelect(item)}>{item}</button>
+              <button type="button" key={item} onClick={() => setFilterSelect(`foundIn: ${item}`)}>{item}</button>
             ))}
           </div>
           <div className="dino-species">
             <h3>Species</h3>
             {filterable.typeSpecies.map((item) => (
-              <button type="button" key={item} onClick={() => setFilterSelect(item)}>{item}</button>
+              <button type="button" key={item} onClick={() => setFilterSelect(`typeSpecies: ${item}`)}>{item}</button>
             ))}
           </div>
 
